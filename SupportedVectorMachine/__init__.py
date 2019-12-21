c1703a5944475ee513f12cec159622cf05fb7039
Wei Xiong <xw931018@gmail.com>
2019-12-21 10:03:35 +0800

    add __init__.py files

---
+++ SupportedVectorMachine/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/12/21 10:02
+# @Author  : wxiong
+# @Site    : 
+# @File    : __init__.py
+# @Desc    : Undocumented .py script created by wxiong
